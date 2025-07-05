825499ad340616d06c296572fb5d08f593738e7c
Thiago <Jay@DESKTOP-TGBBAUH>
2025-07-05 15:47:37 -0300

    adding minor changes to menu

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,19 +7,24 @@ namespace CalculatorCMD
     {
         enum Menu
         {
-            Add,
-            Subtract,
-            Multiply,
-            Divide,
-            Power,
-            SquareRoot,
-            Exit
+            Add = 1,
+            Subtract = 2,
+            Multiply = 3,
+            Divide = 4,
+            Power = 5,
+            SquareRoot = 6,
+            Exit = 7
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Calculator!");
             Console.WriteLine("Please select an operation:");
+            Console.WriteLine();
             Console.WriteLine("1. Add\n2.Subtract\n3.Multiply\n4.Divide\n5.Power\n6.SquareRoot\n7.Exit");
+
+            Menu option = (Menu)int.Parse(Console.ReadLine());
+
+            Console.Write(option);
         }
     }
 }
