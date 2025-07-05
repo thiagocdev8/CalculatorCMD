57f42043b85fb41928cf6e7b33667aa82d903c12
Thiago <Jay@DESKTOP-TGBBAUH>
2025-07-05 15:32:47 -0300

    setting up menu

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,13 +1,25 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace CalculatorCMD
 {
     class Program
     {
-
+        enum Menu
+        {
+            Add,
+            Subtract,
+            Multiply,
+            Divide,
+            Power,
+            SquareRoot,
+            Exit
+        }
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Welcome to the Calculator!");
+            Console.WriteLine("Please select an operation:");
+            Console.WriteLine("1. Add\n2.Subtract\n3.Multiply\n4.Divide\n5.Power\n6.SquareRoot\n7.Exit");
         }
     }
 }
